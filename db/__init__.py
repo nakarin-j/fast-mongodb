8d48f2a5d62a42f297c307a15c735c0bb36e5fc4
mfec_nakarin <nakarin_j@mfec.co.th>
2021-07-26 13:42:15 +0700

    Fix incorrect assign value on set up db

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 from config.default import config
 
-client = MongoClient(config.db_host, config.db_name)
+client = MongoClient(config.db_host, config.db_port)
 db = client['testDb']
